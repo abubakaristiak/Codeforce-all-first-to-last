803106d0810d0c1eaf2090c62611b1c9766f7325
Abu Bakar Istiak <abubakar119147@gmail.com>
2024-06-17 00:12:11 +0600

    add program

---
+++ I_Smallest_Pair.cpp
@@ -0,0 +1,20 @@
+// #include<bits/stdc++.h>
+// using namespace std;
+// int main()
+// {
+//     int n;
+//     cin >> n;
+//     int ar[n];
+//     for(int i=0; i<n; i++){
+//         cin >> ar[i];
+//     }
+//     int ans = 0; 
+//     for(int i=0, i<n, i++){
+//         for(int j=i+1, j<n, j++){
+
+//             int ans = ar[i] + ar[j] + j-i;
+//         }
+//     }
+//     cout << ans << endl;
+//     return 0;
+// }
